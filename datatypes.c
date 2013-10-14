331ab1f8f440944b7a1bb5355205911be8d29ab8
lokesh <lokesh@localhost.localdomain>
2013-10-14 08:40:11 +0530

    New file testing...

---
+++ datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+main()
+{
+	printf("Integer max value: %d\n", INT_MAX);
+	printf("Float max value: %d\n", FLT_MAX);
+	printf("Float max value: %d\n", FLT_MAX_10_EXP);
+	
+	return 0;
+}
